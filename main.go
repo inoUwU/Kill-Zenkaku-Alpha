4713eb82f4a350bda9c7ac56d70c57f3f13a8b23
ino <87593133+inoUwU@users.noreply.github.com>
2022-09-21 00:26:15 +0900

    ♻️ 🐛 cpu使用率を下げる・リファクタリング

--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 	"time"
 )
 
+var (
+	user32              = syscall.NewLazyDLL("user32.dll")
+	imm32               = syscall.NewLazyDLL("imm32.dll")
+	sendMessage         = user32.NewProc("SendMessageA")
+	immGetDefaultIMeWNd = imm32.NewProc("ImmGetDefaultIMEWnd")
+	getForegroundWindow = user32.NewProc("GetForegroundWindow")
+)
+
 const (
-	WM_IME_CONTROL        int = 0x283
-	IMC_GETCONVERSIONMODE int = 1
-	IMC_GETOPENSTATUS     int = 5
-	IMC_SETCONVERSIONMODE int = 6
-	IME_ZENKAKU_Alpha     int = 8
+	IME_CONTROL       int = 0x283
+	GETCONVERSIONMODE int = 1
+	GETOPENSTATUS     int = 5
+	SETCONVERSIONMODE int = 6
+	ZENKAKU_ALPHA     int = 8
 )
 
 func main() {
@@ -47,58 +55,29 @@ func getIcon(s string) []byte {
 func killing() {
 	mKill := systray.AddMenuItem("Stop Kill", "Stop Kill")
 
-	user32, err := syscall.LoadDLL("user32.dll")
-	if err != nil {
-		panic(err)
-	}
-	defer user32.Release()
-
-	imm32, err := syscall.LoadDLL("imm32.dll")
-	if err != nil {
-		panic(err)
-	}
-	defer imm32.Release()
-
-	procGetForegroundWindow, err := user32.FindProc("GetForegroundWindow")
-	if err != nil {
-		panic(err)
-	}
-
-	hwnd, _, _ := procGetForegroundWindow.Call()
-
-	immGetDefaultIMeWNd, err := imm32.FindProc("ImmGetDefaultIMEWnd")
-	if err != nil {
-		panic(err)
-	}
-
-	imwd, _, _ := immGetDefaultIMeWNd.Call(hwnd)
-
-	sendMessage, err := user32.FindProc("SendMessageA")
-	if err != nil {
-		panic(err)
-	}
-
 	handleImm := func() {
-		imeConvMode, _, _ := sendMessage.Call(imwd, uintptr(WM_IME_CONTROL), uintptr(IMC_GETCONVERSIONMODE), uintptr(0))
-		imeState, _, _ := sendMessage.Call(imwd, uintptr(WM_IME_CONTROL), uintptr(IMC_GETOPENSTATUS), uintptr(0))
+		hwnd, _, _ := getForegroundWindow.Call()
+		imwd, _, _ := immGetDefaultIMeWNd.Call(hwnd)
+		imeConvMode, _, _ := sendMessage.Call(imwd, uintptr(IME_CONTROL), uintptr(GETCONVERSIONMODE), uintptr(0))
+		imeState, _, _ := sendMessage.Call(imwd, uintptr(IME_CONTROL), uintptr(GETOPENSTATUS), uintptr(0))
 
 		imeEnabled := imeState != 0
-
-		if imeEnabled && imeConvMode == uintptr(IME_ZENKAKU_Alpha) {
-			sendMessage.Call(imwd, uintptr(WM_IME_CONTROL), uintptr(IMC_SETCONVERSIONMODE), uintptr(0))
+		println(imeState)
+		if imeEnabled && imeConvMode == uintptr(ZENKAKU_ALPHA) {
+			sendMessage.Call(imwd, uintptr(IME_CONTROL), uintptr(SETCONVERSIONMODE), uintptr(0))
 		}
 	}
 
 	run := true
 	toggle := func() {
 		if run {
-			mKill.SetTitle("Stop Kill")
-			mKill.SetTooltip("Stop kill")
+			mKill.SetTitle("Start Kill")
+			mKill.SetTooltip("Start kill")
 
 			run = false
 		} else {
-			mKill.SetTitle("Start Kill")
-			mKill.SetTooltip("Start kill")
+			mKill.SetTitle("Stop Kill")
+			mKill.SetTooltip("Stop kill")
 			run = true
 		}
 	}
@@ -110,12 +89,12 @@ func killing() {
 		}
 	}()
 
-	ticker := time.NewTicker(time.Millisecond * 100)
-	defer ticker.Stop()
 	go func() {
 		for {
 			if run {
 				handleImm()
+				// 0.25秒
+				time.Sleep(time.Millisecond * 250)
 			}
 		}
 	}()
